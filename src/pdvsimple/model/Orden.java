f00849995bc69b050fed2bf1501db746f24fbd03
Kenneth S. Burgos <kenneth.burgos@gmail.com>
2013-11-05 20:31:38 -0400

    Validacion una orden debe ser entregada antes de facturar y no puede ser eliminada si esta facturada.

--- src/pdvsimple/model/Orden.java
+++ src/pdvsimple/model/Orden.java
@@ -2,17 +2,31 @@ package pdvsimple.model;
 
 import javax.persistence.*;
 
+import org.hibernate.validator.*;
 import org.openxava.annotations.*;
+import org.openxava.util.*;
+
+/***
+ * Antes de facturar una orden la orden debe estar entregada
+ * @author
+ * 
+ */
 
 @Entity
 @Views({
-		@View(extendsView = "super.DEFAULT", members = "factura { factura }"),
+		@View(extendsView = "super.DEFAULT", members = "entregado; factura { factura }"),
 		@View(name = "NoClienteNoFactura", members = "ano, codigo, fecha; detalles; notas") })
 public class Orden extends DocumentoComercial {
 
 	@ManyToOne
 	@ReferenceView("NoClienteNoOrdenes")
 	private Factura factura;
+	
+	/* 9.1 - 142 delivered como alternativas de validacion
+	 * Un usuario no puede asignar pedidos a una factura 
+	 * si los pedidos no han sido entregados
+	 */
+	private boolean entregado;
 
 	public Factura getFactura() {
 		return factura;
@@ -20,6 +34,28 @@ public class Orden extends DocumentoComercial {
 
 	public void setFactura(Factura factura) {
 		this.factura = factura;
+	}
+	
+	public boolean isEntregado() {
+		return entregado;
+	}
+	
+	public void setEntregado(boolean entregado) {
+		this.entregado = entregado;
+	}
+	
+	@AssertTrue		//antes de grabar confirma que el metodo devuelve true, si no lanza una excepcion
+	private boolean isEntregadoParaSerFacturado() {
+		return factura == null || isEntregado();	//la logica de validacion
+	}
+	
+	@PreRemove		//justo antes de borrar la entidad
+	private void validateOnRemove() {
+		if (factura != null) {		//la logica de validacion
+			throw new IllegalStateException(	//lanza una excepcion runtime
+				XavaResources.getString(		//para obtener un mensaje de texto i18n
+						"no_se_puede_eliminar_orden_con_factura"));
+		}
 	}
 
 }
